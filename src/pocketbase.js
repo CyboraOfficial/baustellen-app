1d5e7a16212feceac3296f408f25bff4c1674f40
CyboraOfficial <felixweber573@gmail.com>
2026-04-30 23:46:35 +0200

    Add GitHub Actions workflow for building and releasing Electron app

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 // Während der Entwicklung am PC:
-export const pb = new PocketBase("/");
+export const pb = new PocketBase("http://123.456.78.90");
 
 // Später, wenn dein VPS fertig ist, änderst du es zu:
 // export const pb = new PocketBase('https://deine-vps-domain.de');
