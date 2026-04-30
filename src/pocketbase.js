8d4db32470cca4de314fcf2710258439bac07e2e
CyboraOfficial <felixweber573@gmail.com>
2026-05-01 00:25:42 +0200

    Refactor Content Security Policy in index.html and update PocketBase URL in pocketbase.js

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 // Während der Entwicklung am PC:
-export const pb = new PocketBase("http://123.456.78.90");
+export const pb = new PocketBase("http://82.165.109.54");
 
 // Später, wenn dein VPS fertig ist, änderst du es zu:
 // export const pb = new PocketBase('https://deine-vps-domain.de');
