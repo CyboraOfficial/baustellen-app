a7eeb9582268db495345b0e40b129a5a2836af47
CyboraOfficial <felixweber573@gmail.com>
2026-05-02 13:52:41 +0200

    Update app name in manifest, bump version to 1.0.7, and configure PocketBase URL for development and production

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,4 +1,8 @@
 import PocketBase from 'pocketbase';
 
 // Während der Entwicklung am PC:
-export const pb = new PocketBase("https://app.elektro-hegener.de");
+export const pb = new PocketBase(
+  import.meta.env.MODE === 'development' 
+    ? "http://127.0.0.1:8090" 
+    : "https://app.elektro-hegener.de"
+);
