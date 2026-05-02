182555a4b5f822f8650bca8e806d1db6dd3aecfd
CyboraOfficial <felixweber573@gmail.com>
2026-05-02 13:17:19 +0200

    Update deployment target and configure PocketBase URL for production

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,7 +1,4 @@
 import PocketBase from 'pocketbase';
 
 // Während der Entwicklung am PC:
-export const pb = new PocketBase("http://82.165.109.54");
-
-// Später, wenn dein VPS fertig ist, änderst du es zu:
-// export const pb = new PocketBase('https://deine-vps-domain.de');
+export const pb = new PocketBase("https://app.elektro-hegener.de");
